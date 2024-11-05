cb8d7ea82ba09fa1ba3fd2e5debd811ae71245e2
corbinr16 <cgriemenschneider@gmail.com>
2024-11-05 11:39:09 -0600

    LAB 08 - NEW DEMO Step 1

--- src/main/java/cs112/lab08/HelloApplication.java
+++ src/main/java/cs112/lab08/HelloApplication.java
@@ -1,13 +1,20 @@
 package cs112.lab08;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class HelloApplication extends Application implements EventHandler<ActionEvent> {
 
     //CONSTANTS
 
@@ -22,7 +29,42 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        //removed FXML code, fill this in with components, scene, stage, etc.
+        LoteriaCard cardLogo = new LoteriaCard();
+
+        //SETUP
+        Label titleLabel = new Label("Welcome to EChALE STEM Loteria!");
+        ImageView cardImageView = new ImageView(cardLogo.getImage());
+        Label messageLabel = new Label("Click the button to randomly draw a card.");
+        Button drawCardButton = new Button("Draw Random Card");
+
+        //CUSTOMIZATION
+        cardImageView.setFitWidth(300);
+        cardImageView.setPreserveRatio(true);
+        titleLabel.setFont(new Font(20.0));
+
+        //COMPONENTS + LAYOUT
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(titleLabel, cardImageView, messageLabel, drawCardButton);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setSpacing(5.0);
+
+        //SCENE + SHOW
+        Scene scene = new Scene(vbox, 350, 500);
+        stage.setScene(scene);
+        stage.setTitle("EChALE STEM Loteria");
+        stage.show();
+
+
+
+
+
+
+
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+
     }
 
     public static void main(String[] args) {
